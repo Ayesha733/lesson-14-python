8eea521f27aa8d01df902830a6775fdc5faab558
Ayesha733 <h.ayeshasaddiqua@gmail.com>
2026-01-13 08:36:35 -0700

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,7 @@
+def wishes(name):
+    print("Hello", name)
+    print("Welcome to python class \n")
+
+wishes("Ayesha")
+wishes("Amna")
+wishes("Ayat")
---
+++ a2.py
@@ -0,0 +1,7 @@
+def weather (w,c):
+    print("Todays weather is " , w)
+    print("Its soo much",c,"\n")
+
+weather("sunny","hot")
+weather("rainy","cold")
+weather("cloudy","cool")
